bcf738114d201dae35814b8ccf7e9d034dd61053
victoria echevarria <viclee953@gmail.com>
2017-10-19 09:18:15 -0400

    works

--- works.php
+++ works.php
@@ -7,8 +7,6 @@ include("inc/nav.php");
 <section class="worksContainer">
 <h1 class="workh1">Works</h1>
         <p class="workContentIntro">Here you will find some assignments Victoria completed while in Road to Hire</p>
-<h2 class="workContent"><a href="http://vechevarria.road2hire.ninja/guessinggame/" target="_blank">Guess a Random Number</a></h2>
-    <p class="workContent">Guess a randomly generated number</p>
 <h2 class="workContent"><a href="http://vechevarria.road2hire.ninja/commentapp/" target="_blank">Comment App</a>
     <p class="workContent">React App that allows users to leave comments, like or dislike comments, and reply to comments</p>
     <h2 class="workContent"><a href="http://vechevarria.road2hire.ninja/finalphp/" target="_blank">E-commerce Site</a>
