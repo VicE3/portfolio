be1f7ade897ad1d4c0e44e2b6e716b056ceaa050
victoria echevarria <viclee953@gmail.com>
2017-10-24 18:00:33 -0400

    works

--- works.php
+++ works.php
@@ -7,10 +7,18 @@ include("inc/nav.php");
 <section class="worksContainer">
 <h1 class="workh1">Works</h1>
         <p class="workContentIntro">Here you will find some assignments Victoria completed while in Road to Hire</p>
-<h2 class="workContent"><a href="http://vechevarria.road2hire.ninja/commentapp/" target="_blank">Comment App</a>
-    <p class="workContent">React App that allows users to leave comments, like or dislike comments, and reply to comments</p>
-    <h2 class="workContent"><a href="http://vechevarria.road2hire.ninja/finalphp/" target="_blank">E-commerce Site</a>
-    <p class="workContent">An E-commerce site</p>   
+<h2 class="workContent"><a href="http://vechevarria.road2hire.ninja/finalphp/" target="_blank">E-commerce Site</a></h2>
+    <p class="workContent">An E-commerce site</p> 
+<h2 class="workContent"><a href="http://vechevarria.road2hire.ninja/commentapp/" target="_blank">Comment App</a></h2>
+    <p class="workContent">React App that allows users to leave comments, like or dislike comments, and reply to comments</p>  
+<h2 class="workContent"><a href="http://vechevarria.road2hire.ninja/guessinggame/" target="_blank">Guessing Game</a></h2>
+    <p class="workContent">A React App. Choose Standard Game Mode and guess a random number 1-10, with Expert Game Mode guess a number between 1-100</p>
+<h2 class="workContent"><a href="http://vechevarria.road2hire.ninja/contact/" target="_blank">Contact List</a></h2>
+    <p class="workContent">A React App. List of people to be contacted.</p>
+<h2 class="workContent"><a href="http://vechevarria.road2hire.ninja/translator/" target="_blank">Translator App</a></h2>
+    <p class="workContent">A React App. Practice translating words into different languages.</p>       
+<h2 class="workContent"><a href="http://vechevarria.road2hire.ninja/teams/" target="_blank">Team App</a></h2>
+    <p class="workContent">A React App. Assign a group of people into two different teams.</p>
 </section>
 
 <?php 
